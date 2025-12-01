383a4b63c6e9d1cbb6b47b78daaae92246369d1f
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2025-12-01 22:32:09 +0700

    list python

--- list.py
+++ list.py
@@ -21,3 +21,46 @@ makanan.append('cakue')
 # makanan.prepend('siomay') ~ tidak bisa digunakan
 makanan.insert(2, 'sempol')
 print(makanan)
+
+# menghapus list 
+store = ['baju', 'celana', 'kemeja', 'kaos']
+for i in store:
+    print('stock baju {}'. format(i))
+print ("=" * 10)
+
+del store[2]
+for i in store:
+    print('stock toko sekarang {}' . format(i))
+    
+print ("=" * 10)
+store.remove('baju')
+for pi in store:
+    print('stock tersedia di toko {}' . format(i))
+
+# Memotong list
+huruf = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print (huruf[1:5])
+
+# penggabungan list
+sayuran = ['bayam', 'kangkung', 'kobis']
+buah = ['apel', 'sirsak']
+jusCampuran = sayuran + buah
+
+print(jusCampuran)
+for i in jusCampuran:
+    print('macam-macam jus {}' . format(i))
+
+# perkalian list
+jusSayur = sayuran * 5
+print(jusSayur)
+
+
+# list multidimensi
+list_minuman = [
+    ['teh', 'jeruk', 'kopi'],
+    ['es teh', 'es jeruk', 'es kopi'],
+    ['teh anget', 'jeruk anget', 'kopi anget']
+]
+print(list_minuman[1][0])
+for i in list_minuman:
+    print('menu minuman {}' . format(i))
